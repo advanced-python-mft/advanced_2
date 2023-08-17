1f6542471c087fe240ccbc7575f649f2cbe71466
pd <kingdeath1383.com>
2023-08-17 17:44:56 +0330

    haaaaaaaa

--- mamd.py
+++ mamd.py
@@ -1,1 +1,18 @@
-print ('my name is parham')
+import pygame
+
+pygame.init()
+
+dis = pygame.display.set_mode((400,300))
+pygame.display.update()
+RED = (255, 30, 55)
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    pygame.draw.rect(dis, RED, [260, 140, 60, 60])
+    pygame.display.update()
+
+pygame.quit()
+quit()
